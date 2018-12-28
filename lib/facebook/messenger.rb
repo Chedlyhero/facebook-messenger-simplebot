6e6acb81a576bcd4058370aa1e18a4bac76587ec
Chedlyhero <40200800+Chedlyhero@users.noreply.github.com>
2018-12-28 10:20:50 +0100

    Update messenger.rb

--- lib/facebook/messenger.rb
+++ lib/facebook/messenger.rb
@@ -7,6 +7,7 @@ require 'facebook/messenger/server'
 require 'facebook/messenger/server_no_error'
 require 'facebook/messenger/configuration'
 require 'facebook/messenger/incoming'
+require 'facebook/messenger/handover'
 
 module Facebook
   # All the code for this gem resides in this module.
