9be335002fc2a85f10cfbacc77fe738bdd7431ce
Chedlyhero <40200800+Chedlyhero@users.noreply.github.com>
2019-01-04 15:04:23 +0100

    Update server.rb

--- lib/facebook/messenger/server.rb
+++ lib/facebook/messenger/server.rb
@@ -29,7 +29,7 @@ module Facebook
         @response = Rack::Response.new
 
 	puts "SERVER********"
-	puts receive.standby
+	puts receive.standby["message"]
 
         if @request.get?
           verify
