ea6092bb79b0d35922ab604820005023c5e77305
Chedlyhero <40200800+Chedlyhero@users.noreply.github.com>
2019-01-11 14:47:37 +0100

    Update bot.rb

--- lib/facebook/messenger/bot.rb
+++ lib/facebook/messenger/bot.rb
@@ -49,8 +49,7 @@ module Facebook
         # or raises an exception if it was not.
         def deliver(message, access_token:)
           puts "BOT ************"
-          puts FacebookMessengerService.getPersonaId
-          unless FacebookMessengerService.getPersonaId.empty?
+          unless FacebookMessengerService.getPersonaId.nil?
             message << {persona_id: "#{FacebookMessengerService.getPersonaId}"}
             puts message
           end
