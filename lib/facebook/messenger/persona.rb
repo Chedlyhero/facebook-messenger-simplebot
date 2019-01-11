f87888b6f984c53c66299e9861cd6627ddd2e658
Chedlyhero <40200800+Chedlyhero@users.noreply.github.com>
2019-01-11 10:59:30 +0100

    Create persona.rb

---
+++ lib/facebook/messenger/persona.rb
@@ -0,0 +1,31 @@
+require 'httparty'
+
+module Facebook
+  module Messenger
+    module Persona
+      include HTTParty
+
+      base_uri 'https://graph.facebook.com/v3.2/me'
+
+      format :json
+
+      module_function
+
+      def create_persona(settings, access_token:)
+        response = post '/personas', body: settings, query: {
+          access_token: access_token
+        }
+
+        raise_errors(response)
+
+        true
+      end
+
+      def raise_errors(response)
+        raise Error, response['error'] if response.key? 'error'
+      end
+
+      class Error < Facebook::Messenger::FacebookError; end
+    end
+  end
+end
