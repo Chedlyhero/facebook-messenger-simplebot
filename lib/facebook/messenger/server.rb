50e09e4b588a7dea2a6a74b9d987d6d52d24a803
Chedlyhero <40200800+Chedlyhero@users.noreply.github.com>
2019-02-14 16:46:36 +0100

    Update server.rb

--- lib/facebook/messenger/server.rb
+++ lib/facebook/messenger/server.rb
@@ -169,9 +169,10 @@ module Facebook
         events['entry'.freeze].each do |entry|
           # If the application has subscribed to webhooks other than Messenger,
           # 'messaging' won't be available and it is not relevant to us.
-		unless entry['messaging'.freeze]
-			 puts "******* INBOX TAKE CONTROL"
-		end
+	  unless entry['messaging'.freeze]
+	  	puts "******* INBOX TAKE CONTROL"
+ 		puts ['messaging'.freeze]
+	  end
           next unless entry['messaging'.freeze]
           # Facebook may batch several items in the 'messaging' array during
           # periods of high load.
