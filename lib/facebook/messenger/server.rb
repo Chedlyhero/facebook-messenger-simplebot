c3ecf0d8f4b82818ff5abce54a83ef0ec3d3dbca
Chedlyhero <40200800+Chedlyhero@users.noreply.github.com>
2019-02-14 15:15:58 +0100

    Update server.rb

--- lib/facebook/messenger/server.rb
+++ lib/facebook/messenger/server.rb
@@ -1,6 +1,9 @@
 require 'rack'
 require 'json'
 require 'openssl'
+require 'net/http'
+require 'uri'
+require 'json'
 
 module Facebook
   module Messenger
@@ -171,6 +174,12 @@ module Facebook
           # periods of high load.
           entry['messaging'.freeze].each do |messaging|
             Facebook::Messenger::Bot.receive(messaging)
+	 	puts "******* GET THREAD OWNER"
+                uri = URI.parse("https://graph.facebook.com/v2.6/me/thread_owner?recipient=2059758140732393&access_token=EAAHyobtEwpsBAP1IskoYISDH1u98HHuclGVEC3Fse3EDVpNkEZB3kLIQSbDIZAQMCXynl93oA12EMRxanQ0ALQsC5C95rJPB5dZAfNQM57JhiC2qBFhsVvTZBE28vUXmHWNYYjQGzVx7nZComOQeL3EMkLcUuP9931CpLQcuLhwZDZD")
+                response = Net::HTTP.get(uri)
+                response = JSON.parse(response)
+                current_app_id = response["data"][0]["thread_owner"]["app_id"]
+		puts current_app_id
           end
         end
       end
