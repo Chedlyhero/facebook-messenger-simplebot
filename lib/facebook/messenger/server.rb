6e8fc8e58f8c2c8840607764844367ff7cdcd92b
Chedlyhero <40200800+Chedlyhero@users.noreply.github.com>
2019-02-14 16:47:57 +0100

    Update server.rb

--- lib/facebook/messenger/server.rb
+++ lib/facebook/messenger/server.rb
@@ -171,7 +171,7 @@ module Facebook
           # 'messaging' won't be available and it is not relevant to us.
 	  unless entry['messaging'.freeze]
 	  	puts "******* INBOX TAKE CONTROL"
- 		puts ['messaging'.freeze]
+ 		puts ['messaging'.freeze].inspect
 	  end
           next unless entry['messaging'.freeze]
           # Facebook may batch several items in the 'messaging' array during
