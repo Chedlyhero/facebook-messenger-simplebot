98b352736c051f33d372ec47e239d72a632e91e4
Chedlyhero <40200800+Chedlyhero@users.noreply.github.com>
2019-02-14 15:22:08 +0100

    Update server.rb

--- lib/facebook/messenger/server.rb
+++ lib/facebook/messenger/server.rb
@@ -173,13 +173,17 @@ module Facebook
           # Facebook may batch several items in the 'messaging' array during
           # periods of high load.
           entry['messaging'.freeze].each do |messaging|
-		  puts "******* GET THREAD OWNER"
+		puts "******* GET THREAD OWNER"
                 uri = URI.parse("https://graph.facebook.com/v2.6/me/thread_owner?recipient=2059758140732393&access_token=EAAHyobtEwpsBAP1IskoYISDH1u98HHuclGVEC3Fse3EDVpNkEZB3kLIQSbDIZAQMCXynl93oA12EMRxanQ0ALQsC5C95rJPB5dZAfNQM57JhiC2qBFhsVvTZBE28vUXmHWNYYjQGzVx7nZComOQeL3EMkLcUuP9931CpLQcuLhwZDZD")
                 response = Net::HTTP.get(uri)
                 response = JSON.parse(response)
                 current_app_id = response["data"][0]["thread_owner"]["app_id"]
-		puts current_app_id
-            #Facebook::Messenger::Bot.receive(messaging)
+		if current_app_id == Settings.owner_app_id
+	    	 Facebook::Messenger::Bot.receive(messaging)
+		else
+		  puts "******* INBOX TAKE CONTROL"	
+		end
+            
 	 	
           end
         end
