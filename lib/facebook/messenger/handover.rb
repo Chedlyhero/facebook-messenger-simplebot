14c3ef238188332d5a9023cc3332aee46e84df1c
Chedlyhero <40200800+Chedlyhero@users.noreply.github.com>
2019-02-14 14:29:16 +0100

    Update handover.rb

--- lib/facebook/messenger/handover.rb
+++ lib/facebook/messenger/handover.rb
@@ -31,10 +31,17 @@ module Facebook
         true
       end
       
-     
+     def get_thread_owner(settings, access_token:)
+        response = get '/thread_owner', body: settings, query: {
+          access_token: access_token
+        }
+
+        raise_errors(response)
+
+        true
+      end
     
-      
-      
+     
       def raise_errors(response)
         raise Error, response['error'] if response.key? 'error'
       end
