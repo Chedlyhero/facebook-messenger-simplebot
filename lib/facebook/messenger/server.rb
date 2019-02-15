f5836e3ba9f5bf5a19043e21f3f1f35b861a8f9d
Chedlyhero <40200800+Chedlyhero@users.noreply.github.com>
2019-02-15 10:41:27 +0100

    Update server.rb

--- lib/facebook/messenger/server.rb
+++ lib/facebook/messenger/server.rb
@@ -171,21 +171,21 @@ module Facebook
           # If the application has subscribed to webhooks other than Messenger,
           # 'messaging' won't be available and it is not relevant to us.
 	  messaging =  entry['messaging']
-  	  #puts messaging[0]['sender']
-	  #puts messaging[0]['pass_thread_control']
-	  #puts messaging[0]['pass_thread_control']['new_owner_app_id']
 	  unless defined?(messaging[0]['pass_thread_control']).nil?
 		  pass_thread_control = messaging[0]['pass_thread_control']
 		  unless defined?(pass_thread_control['new_owner_app_id']).nil?
 		  	puts pass_thread_control['new_owner_app_id']
-		  	#puts pass_thread_control.new_owner_app_id
-		  	#puts pass_thread_control[0].new_owner_app_id
 		 	if pass_thread_control['new_owner_app_id'].to_s == Settings.owner_app_id.to_s
-				puts "PASS THREAD CONTROL"	
+				puts "PASS THREAD CONTROL"
+				puts messaging
+				#Bot.deliver({recipient: {id: @sender_id},
+                    		#	message: {text: "Maintenant notre bot reprends la main."},
+                    		#	message_type: "RESPONSE"},
+                    		#	access_token: Settings.facebook_accesss_token)
 		  	end
 		  end
 	  end
-	  puts "/*/*/*/"
+	  puts "*/*/*/***/*"
 	  unless entry['messaging'.freeze]
 	  	puts "******* INBOX TAKE CONTROL"
 		standby = entry['standby']
@@ -195,6 +195,7 @@ module Facebook
 			Contact.where(:facebook_id => sende_id).update(handover_reset: '')
 		end
 	  end
+		
           next unless entry['messaging'.freeze]
           # Facebook may batch several items in the 'messaging' array during
           # periods of high load.
