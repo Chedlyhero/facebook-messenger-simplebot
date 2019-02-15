316cf23defd1942b515697c590d6b5c4cbdb9181
Chedlyhero <40200800+Chedlyhero@users.noreply.github.com>
2019-02-15 10:11:39 +0100

    Update server.rb

--- lib/facebook/messenger/server.rb
+++ lib/facebook/messenger/server.rb
@@ -173,6 +173,9 @@ module Facebook
 	  messaging =  entry['messaging']
   	  puts messaging[0]['sender']
 	  puts messaging[0]['pass_thread_control']
+		unless messaging[0]['pass_thread_control'].nil?
+			puts "PASS THREAD CONTROL"	
+		end
 	  puts "/*/*/*/"
 	  unless entry['messaging'.freeze]
 	  	puts "******* INBOX TAKE CONTROL"
