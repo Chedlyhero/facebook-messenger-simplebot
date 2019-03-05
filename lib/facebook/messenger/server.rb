715fcbabbbb6b452b2b3354d9192cdf7a2e41b75
Chedlyhero <40200800+Chedlyhero@users.noreply.github.com>
2019-03-05 12:12:11 +0100

    Update server.rb

--- lib/facebook/messenger/server.rb
+++ lib/facebook/messenger/server.rb
@@ -167,14 +167,16 @@ module Facebook
         # Facebook may batch several items in the 'entry' array during
         # periods of high load.
 	      
+	      		puts "********* TEST"
+	puts events
+	      
         events['entry'.freeze].each do |entry|
           # If the application has subscribed to webhooks other than Messenger,
           # 'messaging' won't be available and it is not relevant to us.
 		
 	  # WHEN PAGE ADMIN DONE CHATING WITH USER AND CLICK TO DONE BUTTON
 	  messaging =  entry['messaging']
-		puts "********* TEST"
-	puts messaging
+
 	  unless defined?(messaging[0]['pass_thread_control']).nil?
 		  pass_thread_control = messaging[0]['pass_thread_control']
 		  unless defined?(pass_thread_control['new_owner_app_id']).nil?
